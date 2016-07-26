d8bca476c70606fc800198004bcc58952aae990f
Bao <Bao@BAO-PC>
2016-07-26 01:13:50 -0700

    Added CustomerController

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers/Index")]
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Smith", Id = 1},
+                new Customer {Name = "Mary Williams", Id = 2}
+            };
+
+            var customerModel = new Vidly.ViewModels.CustomersViewModel()
+            {
+                Customers = customers
+            };
+            return View(customerModel);
+        }
+    }
+}
