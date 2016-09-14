0c9bf5de89576a11591e1ca12a91e1222be5b59d
Bao <Bao@BAO-PC>
2016-09-13 22:31:14 -0700

    REST for Movie Api Controller

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using AutoMapper;
 using Vidly.Dtos;
+using Vidly.Dtos2;
 using Vidly.Models;
 
 namespace Vidly.App_Start
@@ -12,8 +13,13 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            //Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            //Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(c => c.Id, opt => opt.Ignore()); 
         }
     }
 }
---
+++ Vidly/Dtos2/MovieDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos2
+{
+    public class MovieDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public int? GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Display(Name = "Number In Stock")]
+        //[StockCannotBeZero]
+        //[Range(1,20)]
+        public int NumberInStock { get; set; }
+    }
+}
