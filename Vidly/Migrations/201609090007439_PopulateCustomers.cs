a358b17100b6b3d620b4eec975bc66dfbaa22df4
Bao <Bao@BAO-PC>
2016-09-18 15:29:28 -0700

    Typeahead New.cshtml

--- Vidly/Migrations/201609090007439_PopulateCustomers.cs
+++ Vidly/Migrations/201609090007439_PopulateCustomers.cs
@@ -9,6 +9,13 @@ namespace Vidly.Migrations
         {
             Sql("SET IDENTITY_INSERT Customers ON");
             Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Chris', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Bao', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'John', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Steve', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Jake', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Andrew', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Jayden', 'True', 4, 8/24/2016)");
+            Sql("INSERT INTO Customers (Id, Name, IsSubscribedToNewsLetter, MembershipTypeID, Birthdate) VALUES (4, 'Mary', 'True', 4, 8/24/2016)");
         }
         
         public override void Down()
